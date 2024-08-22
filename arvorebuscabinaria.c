bbe07f612756131e4e1eda0a95c3187d41c6d655
HugoDPSz <128089628+HugoDPSz@users.noreply.github.com>
2024-08-21 23:11:29 -0300

    Update arvorebuscabinaria.c

--- arvorebuscabinaria.c
+++ arvorebuscabinaria.c
@@ -1,114 +1,175 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
-    int data;
-    struct node* right;
-    struct node* left;
-} node;
+typedef struct no{
+    int valor;
+    struct no *direita;
+    struct no *esquerda;
+} no;
 
-node* insert(node* root, int x) {
-    node *aux = (node *)calloc(1, sizeof(node));
+no *inserirNo(no *raiz, int valor){
+    no *aux = (no *)calloc(1, sizeof(no));
 
-    aux->data = x;
-    aux->right = NULL;
-    aux->left = NULL;
+    aux->valor = valor;
+    aux->direita = NULL;
+    aux->esquerda = NULL;
 
-    if (root == NULL) {
-        root = aux;
-    } else {
-        node *current = root;
-        node *parent = NULL;
-        while (1) {
-            parent = current;
-            if (x < current->data) {
-                current = current->left;
-
-                if (current == NULL) {
-                    parent->left = aux;
-                    return root;
-                }
-            } else {
-                current = current->right;
-
-                if (current == NULL) {
-                    parent->right = aux;
-                    return root;
-                }
-            }
+    if(raiz == NULL){
+        return aux;
+    }
+    if(aux->valor < raiz->valor){
+        if(raiz->esquerda == NULL){
+            raiz->esquerda = aux;
+        } else{
+            raiz->esquerda = inserirNo(raiz->esquerda, valor);
+        }
+    } else{
+        if(raiz->direita == NULL){
+            raiz->direita = aux;
+        }else{
+            raiz->direita = inserirNo(raiz->direita, valor);
         }
     }
 
-    return root;
+    return raiz;
 }
 
-void emordem(node* root) {
-    if (root) {
-        emordem(root->left);
-        printf("%d ", root->data);
-        emordem(root->right);
+no *encontrarMinimo(no *raiz) {
+    while (raiz->esquerda != NULL) {
+        raiz = raiz->esquerda;
     }
+    return raiz;
 }
 
-void preordem(node* root) {
-    if (root) {
-        printf("%d ", root->data);
-        preordem(root->left);
-        preordem(root->right);
-    }
-}
 
-void posordem(node* root) {
-    if (root) {
-        posordem(root->left);
-        posordem(root->right);
-        printf("%d ", root->data);
-    }
-}
-
-int procura(node* root, int alvo) {
-    node *current = root;
-    printf("\nPassando pelos elementos: ");
-
-    while (current) {
-        printf("%d ", current->data);
-
-        if (alvo < current->data) {
-            current = current->left;
-        } else if (alvo > current->data) {
-            current = current->right;
-        } else {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int main() {
-    node *root = NULL;
-    int lista[] = {34, 52, 33, 65, 28, 654, 21, 76, 123, 98, 12, 32};
-
-    for (int i = 0; i < 12; i++) {
-        root = insert(root, lista[i]);
+no *removerNo(no *raiz, int valor) {
+    if (raiz == NULL) {
+        return NULL;
     }
 
-    printf("\nEm ordem: ");
-    emordem(root);
-
-    printf("\nPré-ordem: ");
-    preordem(root);
-
-    printf("\nPós-ordem: ");
-    posordem(root);
-
-    int alvo = 98;
-
-    int aux = procura(root, alvo);
-    if (aux) {
-        printf("\n[%d] encontrado\n", alvo);
+    if (valor < raiz->valor) {
+        raiz->esquerda = removerNo(raiz->esquerda, valor);
+    } else if (valor > raiz->valor) {
+        raiz->direita = removerNo(raiz->direita, valor);
     } else {
-        printf("\nNao encontrado\n");
+
+        if (raiz->esquerda == NULL && raiz->direita == NULL) {
+            free(raiz);
+            return NULL;
+        }
+
+        if (raiz->esquerda == NULL) {
+            no *temp = raiz->direita;
+            free(raiz);
+            return temp;
+        } else if (raiz->direita == NULL) {
+            no *temp = raiz->esquerda;
+            free(raiz);
+            return temp;
+        }
+
+        no *temp = encontrarMinimo(raiz->direita);
+        raiz->valor = temp->valor;
+        raiz->direita = removerNo(raiz->direita, temp->valor);
+    }
+    return raiz;
+}
+
+void preordem(no *raiz){
+    if(raiz != NULL){
+        printf("%d ", raiz->valor);
+        preordem(raiz->esquerda);
+        preordem(raiz->direita);
+    }
+}
+
+void emordem(no *raiz){
+    if(raiz != NULL){
+        emordem(raiz->esquerda);
+        printf("%d ", raiz->valor);
+        emordem(raiz->direita);
+    }
+}
+
+void posordem(no *raiz){
+    if(raiz != NULL){
+        posordem(raiz->esquerda);
+        posordem(raiz->direita);
+        printf("%d ", raiz->valor);
+    }
+}
+
+int contaNo(no *raiz){
+    int x, y;
+    if(raiz == NULL){
+        return 0;
     }
 
-    return 0;
+    x = contaNo(raiz->direita);
+    y = contaNo(raiz->esquerda);
+
+    return x + y + 1;
+}
+
+int contaNivel(no *raiz){
+    int x, y;
+    if(raiz == NULL){
+        return 0;
+    }
+
+    x = contaNivel(raiz->direita);
+    y = contaNivel(raiz->esquerda);
+
+    if(x>y){
+        return x + 1;
+    } else{
+        return y + 1;
+    }
+}
+
+int main(){
+    int n, valor, opcao;
+    no *raiz = NULL;
+
+    printf("Deseja inserir quantos nos?\n");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        printf("Digite o valor para inserir:\n");
+        scanf("%d", &valor);
+
+        raiz = inserirNo(raiz, valor);
+    }
+
+    printf("Deseja ver a arvore em:\n1 - Pre ordem\n2 - Em ordem\n3 - Pos ordem\n");
+    scanf("%d", &opcao);
+
+    switch(opcao){
+        case 1:
+            preordem(raiz);
+            break;
+        case 2:
+            emordem(raiz);
+            break;
+        case 3:
+            posordem(raiz);
+            break;
+        default:
+            printf("Opcao invalida\n");
+            break;
+    }
+    printf("\nDeseja remover qual valor?\n");
+    scanf("%d", &valor);
+    removerNo(raiz, valor);
+
+    printf("A alteracao em ordem resulta na seguinte arvore:\n");
+    emordem(raiz);
+
+    int nos = contaNo(raiz);
+    printf("\nA arvore tem %d nos\n", nos);
+
+    int nivel = contaNivel(raiz);
+    printf("A arvore tem nivel %d\n", nivel);
+
+    return 0;    
 }
